39f533120fd10eac82edbf57cc46ebfb637848aa
Afrinfatma <afrinfatma4@gmail.com>
2022-06-14 20:33:45 +0530

    Added : case statement

---
+++ weekdays.sh
@@ -0,0 +1,18 @@
+read -p "enter Day": Day
+case "$Day" in 
+      "Monday") echo "Monday is First working day."
+      ;;
+      "Tuesday") echo "Tuesday is second working day."
+      ;;
+        "Wednesday") echo "Wednesday is second working day."
+      ;;
+        "Thursday") echo "Thursday is second working day."
+      ;;
+        "Friday") echo "Friday is last working day."
+      ;;
+       "Saturday") echo "saturday is weekend."
+      ;;
+        "Sunday") echo "Sunday is Holiday."
+      ;;
+
+esac
